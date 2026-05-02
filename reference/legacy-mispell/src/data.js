e40e13855ed76441eede5dca943ac5ca32841a15
PerStrateg <perstrateg@gmail.com>
2026-05-02 21:11:20 +0600

    Remove reference files from repository

--- reference/legacy-mispell/src/data.js
+++
@@ -1,3 +0,0 @@
-module.exports.dict = require('../res/finalDict.json')
-
-module.exports.common_words = require('../res/20k-common-words.json').list
--- reference/legacy/wordReplace/data/universal.js
+++
@@ -1,8 +0,0 @@
-// Universal replacement data keeps examples broad and language-neutral.
-export const UNIVERSAL_WORD_REPLACE_DATA = {
-   replacements: [
-      [/\btest\b/giu, ["experiment", "sample"], 0.8],
-      [/\btext\b/giu, ["words", "phrase"], 0.45],
-      [/\bpage\b/giu, ["screen", "surface"], 0.35]
-   ]
-};
